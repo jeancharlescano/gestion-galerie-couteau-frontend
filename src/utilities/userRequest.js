019f7140e33428440334c7361665a183046f038a
jeancharlescano <jeancharles.cano@outlook.fr>
2025-04-22 14:40:11 +0200

    fix register

--- src/utilities/userRequest.js
+++ src/utilities/userRequest.js
@@ -1,13 +1,15 @@
 export const register = async (values) => {
-  console.log("🚀 ~ register ~ values:", values);
   try {
-    let response = await fetch(`${process.env.REACT_APP_API_URL}/auth/register`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(values),
-    });
+    let response = await fetch(
+      `${import.meta.env.VITE_REACT_APP_API_URL}/auth/register`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      }
+    );
 
     return response.status;
   } catch (error) {
